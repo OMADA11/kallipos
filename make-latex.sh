548480c6e586073455d6e10b4b61eb9ec11f150e
nkanagno <103074273+nkanagno@users.noreply.github.com>
2023-03-20 03:22:42 +0200

    Update make-latex.sh

--- make-latex.sh
+++ make-latex.sh
@@ -9,6 +9,9 @@ for filename in text/ch*.txt; do
    pandoc --lua-filter=extras.lua "$filename" --to markdown | pandoc --lua-filter=extras.lua --to markdown | pandoc --lua-filter=epigraph.lua --to markdown | pandoc --lua-filter=fun_fact.lua --to markdown | pandoc --lua-filter=figure.lua --to markdown | pandoc --lua-filter=footnote.lua --to markdown | pandoc --filter pandoc-fignos --to markdown | pandoc --metadata-file=meta.yml --top-level-division=chapter --citeproc --bibliography=bibliography/"$(basename "$filename" .txt).bib" --reference-location=section --wrap=none --to latex > latex/"$(basename "$filename" .txt).tex"
 done
 
+pandoc text/web.txt --lua-filter=epigraph.lua --to markdown | pandoc --top-level-division=chapter --to latex > latex/web.tex
+pandoc text/bio.txt --top-level-division=chapter --to latex > latex/bio.tex
+
 
 for filename in text/apx*.txt; do
    [ -e "$filename" ] || continue
